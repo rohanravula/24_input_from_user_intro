cd2bac40374785cb34a2f3b8e9152f0ff49ab4d5
rohanravula <109545469+rohanravula@users.noreply.github.com>
2022-08-19 10:48:56 +0530

    Add files via upload

---
+++ input_from_user_intro.py
@@ -0,0 +1,31 @@
+# a=input()
+# b=input()
+# print(a+b) 
+# print(type(a))
+# print(type(b))
+
+# a=input("Numbers :")
+# b=input("Name : ")
+# print(a+b) #in input we can directly pass the values in the output it self.
+
+"if in input we want to print only the int values then do this"
+# a=int(input("Mobile NO : "))
+# print(a) #now we can pass the only the int values only if we try to write str or float vlaues it will not accept.
+
+
+# a=int(input())
+# b=str(input()) # we want the anwer in int 10. so convert str to int
+# print(a+int(b)) #5+5=10
+
+a=int(input())
+b=str(input()) #we want the answer in the str i,e. 5rohan. so now convert int to str
+print(str(a)+b) #5+rohan=5rohan
+
+
+
+
+
+
+
+
+
